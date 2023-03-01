d62983aeec69a77a21e38c0663b87e40312db74a
Eliana <elianastefanini@hotmail.com>
2023-02-28 23:20:35 -0300

    Primera escritura en el repositorio

---
+++ cv_js.js
@@ -0,0 +1,19 @@
+function cambiarColor(trabajo1){
+var parrafo = document.getElementById('trabajo1')
+parrafo.style.color = 'black'
+}
+
+function reestablecerColor(trabajo1){
+	var parrafo = document.getElementById('trabajo1')
+	parrafo.style.color = '#F2F4F4'
+}
+
+function cambiarColor1(habilidades1){
+var parrafo = document.getElementById('habilidades1')
+parrafo.style.color = 'black'
+}
+
+function reestablecerColor1(habilidades1){
+	var parrafo = document.getElementById('habilidades1')
+	parrafo.style.color = '#F2F4F4'
+}
